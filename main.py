e9e0c4e95e1a4a1037daa17f117a15a78b221919
Woodyfosho <92193413+CodyWoodard@users.noreply.github.com>
2023-08-30 16:53:22 -0500

    Extracted the names out of the filepath for each csv file, and populated a list with them.

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-# Familiarizing myself with csv.reader in Python. Received this from
+# Familiarizing myself with csv.reader/writer in Python. Received this from
 # Pythons docs @ https://docs.python.org/3/library/csv.html
 if __name__ == '__main__':
 
-    import csv
-    with open('BobsParts.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    import os
+    # This block of code was built using:
+    # https://stackoverflow.com/questions/10149263/extract-a-part-of-the-filepath-a-directory-in-python
+    Bobs_Path = os.path.abspath('BobsParts.csv')
+    Insta_Path = os.path.abspath('Instamonia.csv')
+    Vanilla_Path = os.path.abspath('VanillaAndCanolaSpecialists.csv')
 
-        for row in spamreader:
-            print(', '.join(row))
+    List_Of_Files = [Bobs_Path, Insta_Path, Vanilla_Path]  # Populating a list.
+
+    def path_stripper(List_Of_Files):
+        i = 0
+        Parsed_Filenames = []
+        for x in List_Of_Files:
+
+            parent_directory, directory_name = os.path.split(List_Of_Files[i])
+            stripped_path = directory_name.strip('.csv')
+            #print(stripped_path)
+            Parsed_Filenames.append(stripped_path)
+            #print(Parsed_Filenames[i])
+            i += 1
+        return Parsed_Filenames
+
+    #print(path_stripper(List_Of_Files))
 
 
 
