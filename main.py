a21d69ff0c19d3134d57fd905736e659fc43647f
Woodyfosho <92193413+CodyWoodard@users.noreply.github.com>
2023-08-30 21:43:53 -0500

    Created a method that reads & writes csv files dynamically. (not finished).

--- main.py
+++ main.py
@@ -17,16 +17,46 @@ if __name__ == '__main__':
         i = 0
         Parsed_Filenames = []
         for x in List_Of_Files:
-
+            #
             parent_directory, directory_name = os.path.split(List_Of_Files[i])
-            stripped_path = directory_name.strip('.csv')
+            #print(directory_name)
+            # Learning about the strip method:
+            # https://www.freecodecamp.org/news/python-strip-how-to-trim-a-string-or-line/
+            stripped_path = directory_name.strip('.csv')  # Trimming .csv from the string.
             #print(stripped_path)
-            Parsed_Filenames.append(stripped_path)
+            Parsed_Filenames.append(stripped_path)  # Appending the suppliers name to a new list.
             #print(Parsed_Filenames[i])
             i += 1
         return Parsed_Filenames
 
+# Creating a method that will read & write csv files dynamically.
+    def list_of_csv_file_file(List_Of_Files):
+        i = 0
+        import csv
+        for x in List_Of_Files:
+            # Getting my filename.csv assigned to directory_name
+            parent_directory, directory_name = os.path.split(List_Of_Files[i])
+            print(directory_name)
+            i += 1
+            # reading each csv file.
+            with open(directory_name, 'r') as csvfile:
+                csvreader = csv.reader(csvfile)
+                for line in csvreader:
+                    print(line)
+
+        # Once I learn how to save columns to a list I will save the first column
+        # In each csv file to be used later to populate my final csv fle.
+        columns = []
+        for row in csv.reader(directory_name, delimiter=','):
+            columns.append(row[0])
+
+        return columns
+
+    print(list_of_csv_file_file(List_Of_Files))
     #print(path_stripper(List_Of_Files))
 
-
+    #def organize_csv(List_of):
+     #   columns = []
+      #  for row in csv.reader(infile, delimiter="\t"):
+       #     columns.append(row[1])  # here row[1] is the second column
 
