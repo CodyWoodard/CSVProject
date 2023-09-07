1917ba85e85521465670c7bec2511240abd4378b
Woodyfosho <92193413+CodyWoodard@users.noreply.github.com>
2023-09-06 21:45:38 -0500

    Optimised my solution to automatically read new csv files added to a folder, and write the final csv file to a new folder.

--- main.py
+++ main.py
@@ -3,23 +3,29 @@
 
 if __name__ == '__main__':
 
-    import os
     import csv
     import pandas as pd
-    from csv import writer
-    # Assigning csv filenames to variables
-    Bobs = 'BobsParts.csv'
-    Insta = 'Instamonia.csv'
-    Vanilla = 'VanillaAndCanolaSpecialists.csv'
-    Test = 'Test.csv'
+    import os
+    # Will be using glob to retrieve files/pathnames '.csv'
+    import glob
+    #import pyodbc
 
-    List_Of_Files = [Bobs, Insta, Vanilla, Test]  # Populating a list with the csv files.
+    received_files = []
+    # Using os.chdir to change the current working directory to 'Received Files'
+    # https://stackoverflow.com/questions/39838332
+    # /reading-multiple-csv-files-from-different-directories-into-pandas-dataframe
+    os.chdir('Received Files')
+    # the glob module finds all pathnames matching a specified pattern, in this case '.csv'.
+    for files in glob.glob("*.csv"):
+        # Appending the files to the list 'fileList'
+        received_files.append(files)
 
+    print(received_files)
 
-    def Organize_CSV(list_of_files):
+    def Organize_CSV(received_files):
         # Using i as an iterative variable within my method.
         i = 0
-        import csv
+        #import csv
         # Downloaded and installed Pandas to be used for the data within this script.
         import pandas as pd
 
@@ -28,19 +34,16 @@ if __name__ == '__main__':
         price = []
         addName = []
 
-
-
-
-        for x in list_of_files:
+        for x in received_files:
             # Grabbing the filename each time my loop iterates.
-            file_name = List_Of_Files[i]
+            file_name = received_files[i]
 
             # Strip method:
             # https://www.freecodecamp.org/news/python-strip-how-to-trim-a-string-or-line/
             stripped_path = file_name.strip('.csv')  # Trimming .csv from the string.
 
             # I am reading in my csv files and assigning them to df.
-            df = pd.read_csv(List_Of_Files[i])
+            df = pd.read_csv(received_files[i])
             # newSKU is a list to hold the contents of column[0] (SKUs), however, without the
             # .values method I couldn't write the contents of newSKU to a new file correctly
             # because I was creating a list of lists, when I only needed the values of column 0 to populate a list
@@ -58,7 +61,7 @@ if __name__ == '__main__':
 
             # I am certain there is a better way to do this using Pandas, but this does work.
             # Reading the csv file in and assigning it to fileObject.
-            fileObject = open(List_Of_Files[i], 'r')
+            fileObject = open(received_files[i], 'r')
             # DictReader is mapping the information in each row to a dict,
             # whose keys are given by the optional fieldnames parameter.
             # https://docs.python.org/3/library/csv.html#csv.DictReader
@@ -74,16 +77,20 @@ if __name__ == '__main__':
         # https://www.geeksforgeeks.org/python-save-list-to-csv/
         # Okay, 'SKU' 'PRICE' & 'SUPPLIER' are my keys, which will be used to populate the corresponding columns.
 
-        dict = {'SKU': SKU, 'PRICE': price,'SUPPLIER': addName}
+        dict = {'SKU': SKU, 'PRICE': price, 'SUPPLIER': addName}
         # Adding to the dataframe.
         df = pd.DataFrame(dict)
         # Writing to csv.
         # Setting SKU as my index, which removes the indexing column that was before my data.
         df = df.set_index('SKU')
-        df.to_csv('newFile.csv')
+        df.to_csv(r"C:/Users/codyw/PycharmProjects/CSV Project/Final CSV File/newFile.csv")
+
 
 
         return "Done, check out 'newFile.csv'"
+    #pyodbc.drivers()
 
-    print(Organize_CSV(List_Of_Files))
+    #con = pyodbc.connect(Trusted_Connection = "Yes",)
+    print(Organize_CSV(received_files))
+    print('Opening acess..')
 
